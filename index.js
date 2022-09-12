979167b4d6a0502fdf2810c60d53d3734c792aed
neel123go <ayounpaul1029@gmail.com>
2022-09-12 10:36:49 +0600

    created api's for admin role

--- index.js
+++ index.js
@@ -135,6 +135,31 @@ async function run() {
             res.send(reviews);
         });
 
+        // get all users
+        app.get('/user', async (req, res) => {
+            const users = await usersCollection.find().toArray();
+            res.send(users);
+        })
+
+        // make user admin
+        app.put("/user/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const user = await usersCollection.updateOne(filter, updateDoc);
+            res.send(user);
+        });
+
+        // check admin role
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
+
     } finally {
         //   await client.close();
     }
